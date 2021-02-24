77402109374df7e4ccdafdb8b0d2e66c16ddd5a6
TotallyNotRust <35370673+TotallyNotRust@users.noreply.github.com>
2021-02-24 09:40:03 +0100

    .

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ logging.basicConfig(filename='log.log', encoding='utf-8', level=logging.DEBUG)
 
 app = Flask(__name__)
 
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
 if any([i.startswith('rS') or i.startswith('randomString') for i in sys.argv]):
     key = secrets.token_hex(16)
     app.config['SECRET_KEY'] = secrets.token_hex(16)
@@ -28,18 +31,22 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 print(key)
 print(app.config['SECRET_KEY'])
  
-def getPosts():
-
+def getPosts(ID = None):
     conn = sql.connect("posts.db")
     c = conn.cursor()
-    c.execute("SELECT * FROM POSTS")
-    posts = [{"title": t, "content": c, "images": ["\static\\"+i for i in i.split(",") if i!='']} for t, c, i in c.fetchall()]
+    if ID:
+        c.execute("SELECT * FROM POSTS WHERE id==?", (int(ID), ))
+    else:
+        c.execute("SELECT * FROM POSTS")
+    print(ID)
+    posts = [{"title": t, "content": c, "images": ["\static\\"+i for i in i.split(",") if i!=''], "id": I} for t, c, i, I in c.fetchall()]
     return posts
 
 def submitPost(title, body, picture):
     conn = sql.connect("posts.db")
     c = conn.cursor()
-    c.execute("INSERT INTO POSTS VALUES (?, ?, ?)", (title, body, picture))
+    c.execute("SELECT * FROM posts")
+    c.execute("INSERT INTO POSTS VALUES (?, ?, ?, ?)", (title, body, picture, len(c.fetchall())))
     conn.commit()
 
 @app.route("/removeKey")
@@ -48,20 +55,71 @@ def removeKey():
     resp.set_cookie('key', '')
     return resp
 
+@app.route("/editPost", methods=['GET', 'POST'])
+def editPost():
+    if request.cookies.get("key") == hashlib.md5((key+request.remote_addr).encode()).hexdigest():
+        form = MakePost()
+        conn = sql.connect("posts.db")
+        c = conn.cursor()
+        if form.validate_on_submit():
+            ID = request.args.get("id")
+            
+            if form.pictures.data:
+                picture = save_image(form.pictures.data)
+            else:
+                picture = ""
+            flash('Post made.', 'success')
+            c.execute("UPDATE posts SET title=?, content=?, pictures=? WHERE id==?", (form.title.data, form.body.data, picture, int(ID),))
+            conn.commit() 
+            return redirect(url_for('mainPage'))
+        else:
+            Post = getPosts(request.args.get("id"))[0]
+            form.title.data, form.body.data, picture, ID = Post["title"],Post["content"],Post["images"],Post["id"]
+            return render_template("makePost.html", title="Make post", form=form)
+    else:
+        return page_not_found('')
+
+@app.route("/post")
+def fullPost():
+    form = MakePost()
+    conn = sql.connect("posts.db")
+    c = conn.cursor()
+    post = getPosts(request.args.get("id"))[0]
+    code = {str(post["id"]): post["content"].split("``")[1::2]}
+    post = post | {"content":"--code".join(post["content"].split("``")[::2])} 
+    return render_template("fullPagePost.html", title="Make post", post=post, code=code)
+
+@app.route("/delete")
+def deletePost():
+    if request.cookies.get("key") == hashlib.md5((key+request.remote_addr).encode()).hexdigest():
+        ID = request.args.get("id")
+        conn = sql.connect("posts.db")
+        c = conn.cursor()
+        c.execute("DELETE FROM posts WHERE id==?", (int(ID),))
+        conn.commit()
+        c.execute("SELECT * FROM posts")
+        for indent, i in enumerate(c.fetchall()):
+            print(i[3], ":", str(indent))
+            c.execute("UPDATE posts SET id=? WHERE id==?", (indent, i[3]))
+        conn.commit()
+        return redirect(url_for('mainPage'))
+    else:
+        return page_not_found('')
+
 @app.route("/")
 def mainPage():
-
     [print(f'{i}: {request.cookies.get(i)}') for i in request.cookies]
     if not request.cookies.get('seenCookies') == "1":
         flash("This site uses cookies to keep track of users, none of this data is stored or sold.")
     posts = getPosts()
     print(posts)
 
-    code = [i["content"].split("``")[1::2] for i in posts]
+    code = {str(i["id"]): i["content"].split("``")[1::2] for i in posts}
     
-    print([{"content":"--code".join(i["content"].split("``")[::2])} for i in posts])
+    #print([{"content":"--code".join(i["content"].split("``")[::2])} for i in posts])
     posts = [i | {"content":"--code".join(i["content"].split("``")[::2])} for i in posts]
-
+    print(code)
+    #print(code["1"])
     resp = make_response(render_template("html.html", title="Gustav's blog", posts=posts, code=code, isAdmin=request.cookies.get('key')==hashlib.md5((key+request.remote_addr).encode()).hexdigest()))
     resp.set_cookie('seenCookies', "1")
     return resp
--- manual-add-post.py
+++ manual-add-post.py
@@ -5,18 +5,20 @@ c = conn.cursor()
 
 print(path.exists("./static/arduino.jpg"))
 
-#c.execute("CREATE TABLE POSTS (title, content, pictures)")
+c.execute("CREATE TABLE POSTS (title, content, pictures, id)")
 
 #c.execute("INSERT INTO POSTS VALUES (?, ?, ?)", (input("Title: "), input("Content: "), ",".join([i for i in input("Images (Seperated by ,): ").split(",") if path.exists("./static/"+i)])))
 
-c.execute("DELETE FROM POSTS WHERE title==?", ("Code :)",))
+#c.execute("DELETE FROM POSTS WHERE title==?", ("Code :)",))
+def getPost():
+    c.execute("SELECT * FROM POSTS")
+    posts = [{"title": t, "content": c, "hasImages": i!='', "images": [i for i in i.split(",")], "id": I} for t, c, i, I in c.fetchall()]
+    print(posts)
 
-c.execute("SELECT * FROM POSTS")
-posts = [{"title": t, "content": c, "hasImages": i!='', "images": [i for i in i.split(",")]} for t, c, i in c.fetchall()]
-print(posts)
+getPost()
 
-c.execute("SELECT * FROM POSTS")
-for i in c.fetchall():
-    print(i)
+c.execute("UPDATE posts SET id=? WHERE id==?", ("0", "1"))
+
+getPost()
 
 conn.commit()
